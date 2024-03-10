a8fdd2a0dc72b157307cec25aba1e3def6d911bb
Bezniak <ivan.bezniak@gmail.com>
2024-03-10 12:59:10 +0300

    ReviewForm.jsx

--- src/components/Reviews/ReviewForm/ReviewForm.jsx
+++ src/components/Reviews/ReviewForm/ReviewForm.jsx
@@ -1,28 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import {useForm} from 'react-hook-form';
+import {NavLink} from 'react-router-dom';
+import {FaRegStar} from 'react-icons/fa6';
+import axios from 'axios';
 import styles from './ReviewForm.module.css';
-import {NavLink} from "react-router-dom";
-import {animateScroll as scroll} from "react-scroll";
-import {FaRegStar} from "react-icons/fa6";
-import axios from "axios";
 
 function ReviewForm() {
-
     const [rating, setRating] = useState(0);
     const [isFormSend, setIsFormSend] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-
 
     const {
         register,
         reset,
         handleSubmit,
-        clearErrors,
         setError,
+        clearErrors,
         formState: {errors},
         watch,
-        trigger
+        trigger,
     } = useForm();
+
+    const watchCheckbox = watch('agreement', false);
 
     const onSubmit = async (data) => {
         if (rating === 0) {
@@ -33,170 +32,179 @@ function ReviewForm() {
             return;
         }
 
-        setIsLoading(true)
+        setIsLoading(true);
 
         try {
-            const response = await axios.post(process.env.REACT_APP_API_URL + `/comments`, {
-                data: {
-                    ...data,
-                    grade: rating,
-                    publishedAt: null
-                },
+            await axios.post(process.env.REACT_APP_API_URL + `/comments`, {
+                data: {...data, grade: rating, publishedAt: null},
             });
-            setIsFormSend(true)
+            setIsFormSend(true);
         } catch (error) {
             console.error('Error submitting comment:', error);
         } finally {
-            setIsLoading(false)
-            setIsFormSend(true)
+            setIsLoading(false);
+            setIsFormSend(true);
         }
-        reset()
-        setRating(0)
+
+        reset();
+        setRating(0);
     };
 
-    const watchCheckbox = watch('agreement', false);
-
     const handleBlur = async (field) => {
-        // Вызываем проверку валидации после потери фокуса
         await trigger(field);
     };
 
     const handleClick = () => {
-        // Плавный скролл вверх с использованием react-scroll
-        scroll.scrollToTop({
-            duration: 0, // Продолжительность анимации в миллисекундах
-            smooth: 'easeInOutQuad', // Тип анимации
-        });
+        window.scrollTo({top: 0, behavior: 'smooth'});
     };
+
+    const renderGradeStars = useMemo(() => {
+        return Array.from({length: 5}, (_, index) => (
+            <FaRegStar
+                key={index}
+                color={index < rating ? '#ffc107' : 'gray'}
+                size={28}
+                onClick={() => {
+                    setRating(index + 1);
+                    clearErrors('grade');
+                }}
+                onBlur={() => handleBlur('grade')}
+            />
+        ));
+    }, [rating, clearErrors, handleBlur]);
 
 
     return (
         <>
-            {isFormSend
-                ? (<h1 className={styles.reviewTitle}>Спасибо за Ваш отзыв! <br/> Он будет опубликован в ближайшее время после проверки модератором.</h1>)
-                : (
-                    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-                        <div className={styles.field}>
-                            <label>Имя:</label>
+            {isFormSend ? (
+                <h1 className={styles.reviewTitle}>
+                    Спасибо за Ваш отзыв! <br/>
+                    Он будет опубликован в ближайшее время после проверки модератором.
+                </h1>
+            ) : (
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                    <div className={styles.field}>
+                        <label>Имя:</label>
+                        <input
+                            className={styles.input}
+                            {...register('name', {
+                                required: 'Это поле обязательно для заполнения'
+                            })}
+                            onBlur={() => handleBlur('name')}
+                            placeholder='Екатерина'
+                        />
+                        {errors.name && <span className={styles.error}>{errors.name.message}</span>}
+                    </div>
+
+                    <div className={styles.field}>
+                        <label>Телефон:</label>
+                        <input
+                            className={styles.input}
+                            {...register('phone', {
+                                required: 'Это поле обязательно для заполнения',
+                                pattern: {
+                                    value: /^\+?\d{1,3}(\s?|\(\d{1,4}\))?([\s.-]?\d{1,4}){2,}$/,
+                                    message: 'Некорректный формат номера телефона',
+                                },
+                            })}
+                            onBlur={() => handleBlur('phone')}
+                            placeholder='+375 29 333 33 33'
+                        />
+                        <span className={styles.protection}>Укажите номер телефона на который было бронирование. На сайте отображаться не будет</span>
+                        <br/>
+                        {errors.phone && <span className={styles.error}>{errors.phone.message}</span>}
+                    </div>
+
+                    <div className={styles.field}>
+                        <label>Email:</label>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            {...register('email', {
+                                required: 'Это поле обязательно для заполнения',
+                                pattern: {
+                                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                                    message: 'Некорректный формат email-адреса',
+                                },
+                            })}
+                            onBlur={() => handleBlur('email')}
+                            placeholder='kate@gmail.com'
+                        />
+                        <span className={styles.protection}>На сайте отображаться не будет</span>
+                        <br/>
+                        {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+                    </div>
+
+                    <div className={styles.field}>
+                        <label>Отзыв:</label>
+                        <textarea
+                            className={styles.textarea}
+                            {...register('comment', {
+                                required: 'Это поле обязательно для заполнения'
+                            })}
+                            onBlur={() => handleBlur('comment')}
+                            placeholder='Ваш отзыв...'
+                        />
+                        {errors.comment && <span className={styles.error}>{errors.comment.message}</span>}
+                    </div>
+
+                    <div className={styles.field}>
+                        <label htmlFor="grade">Оценка:</label>
+                        {Array.from({length: 5}, (_, index) => (
+                            <FaRegStar
+                                className={styles.grade}
+                                key={index}
+                                color={index < rating ? '#ffc107' : 'gray'}
+                                size={28}
+                                onClick={() => {
+                                    setRating(index + 1);
+                                    clearErrors('grade');
+                                }}
+                                onBlur={() => handleBlur('grade')}
+                            />
+                        ))}
+                        <br/>
+                        {errors.grade && (
+                            <span className={styles.error}>{errors.grade.message}</span>
+                        )}
+                    </div>
+
+
+                    <div className={styles.field}>
+                        <label>
                             <input
-                                className={styles.input}
-                                {...register('name', {
-                                    required: 'Это поле обязательно для заполнения'
+                                className={styles.checkbox}
+                                type="checkbox"
+                                {...register('agreement', {
+                                    required: 'Вы должны согласиться с условиями'
                                 })}
-                                onBlur={() => handleBlur('name')}
-                                placeholder='Екатерина'
                             />
-                            {errors.name && <span className={styles.error}>{errors.name.message}</span>}
-                        </div>
+                            Подтверждаю ознакомление с <NavLink className={styles.link} to='/privacyPolicy'
+                                                                onClick={handleClick}>правами, механизмом их
+                            реализации</NavLink> и даю согласие
+                            на обработку персональных данных с целью рассмотрения, опубликования и анализа отзыва
+                        </label>
+                        {errors.agreement && <span className={styles.error}>{errors.agreement.message}</span>}
+                    </div>
 
-                        <div className={styles.field}>
-                            <label>Телефон:</label>
-                            <input
-                                className={styles.input}
-                                {...register('phone', {
-                                    required: 'Это поле обязательно для заполнения',
-                                    pattern: {
-                                        value: /^\+?\d{1,3}(\s?|\(\d{1,4}\))?([\s.-]?\d{1,4}){2,}$/,
-                                        message: 'Некорректный формат номера телефона',
-                                    },
-                                })}
-                                onBlur={() => handleBlur('phone')}
-                                placeholder='+375 29 333 33 33'
-                            />
-                            <span className={styles.protection}>Укажите номер телефона на который было бронирование. На сайте отображаться не будет</span>
-                            <br/>
-                            {errors.phone && <span className={styles.error}>{errors.phone.message}</span>}
-                        </div>
+                    <button type="submit" className={styles.button}
+                            disabled={!watchCheckbox || isLoading}>{isLoading ? 'Отправка...' : 'Отправить'}</button>
 
-                        <div className={styles.field}>
-                            <label>Email:</label>
-                            <input
-                                className={styles.input}
-                                type="email"
-                                {...register('email', {
-                                    required: 'Это поле обязательно для заполнения',
-                                    pattern: {
-                                        value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-                                        message: 'Некорректный формат email-адреса',
-                                    },
-                                })}
-                                onBlur={() => handleBlur('email')}
-                                placeholder='kate@gmail.com'
-                            />
-                            <span className={styles.protection}>На сайте отображаться не будет</span>
-                            <br/>
-                            {errors.email && <span className={styles.error}>{errors.email.message}</span>}
-                        </div>
-
-                        <div className={styles.field}>
-                            <label>Отзыв:</label>
-                            <textarea
-                                className={styles.textarea}
-                                {...register('comment', {
-                                    required: 'Это поле обязательно для заполнения'
-                                })}
-                                onBlur={() => handleBlur('comment')}
-                                placeholder='Ваш отзыв...'
-                            />
-                            {errors.comment && <span className={styles.error}>{errors.comment.message}</span>}
-                        </div>
-
-                        <div className={styles.field}>
-                            <label htmlFor="grade">Оценка:</label>
-                            {Array.from({length: 5}, (_, index) => (
-                                <FaRegStar
-                                    className={styles.grade}
-                                    key={index}
-                                    color={index < rating ? '#ffc107' : 'gray'}
-                                    size={28}
-                                    onClick={() => {
-                                        setRating(index + 1);
-                                        clearErrors('grade');
-                                    }}
-                                    onBlur={() => handleBlur('grade')}
-                                />
-                            ))}
-                            {errors.grade && (
-                                <span className={styles.error}>{errors.grade.message}</span>
-                            )}
-                        </div>
-
-
-                        <div className={styles.field}>
-                            <label>
-                                <input
-                                    className={styles.checkbox}
-                                    type="checkbox"
-                                    {...register('agreement', {
-                                        required: 'Вы должны согласиться с условиями'
-                                    })}
-                                />
-                                Подтверждаю ознакомление с <NavLink className={styles.link} to='/privacyPolicy'
-                                                                    onClick={handleClick}>правами, механизмом их
-                                реализации</NavLink> и даю согласие
-                                на обработку персональных данных с целью рассмотрения, опубликования и анализа отзыва
-                            </label>
-                            {errors.agreement && <span className={styles.error}>{errors.agreement.message}</span>}
-                        </div>
-
-                        <button type="submit" className={styles.button}
-                                disabled={!watchCheckbox || isLoading}>{isLoading ? 'Отправка...' : 'Отправить'}</button>
-
-                        <p className={styles.info}>
-                            Убедительная просьба: если ваш отзыв содержит претензию к качеству оказываемой услуги или
-                            сервису,
-                            пожалуйста, укажите телефон, оставленный вами при бронировании, чтобы мы могли уточнить
-                            обстоятельства и
-                            дать
-                            конкретный ответ по вашему вопросу. Мы гарантируем конфиденциальность ваших личных данных.
-                            Благодарим за
-                            понимание.
-                        </p>
-                        <p className={styles.info2}>
-                            Обработка отзывов осуществляется только в рабочие дни и может занять до 7 дней.
-                        </p>
-                    </form>
-                )
+                    <p className={styles.info}>
+                        Убедительная просьба: если ваш отзыв содержит претензию к качеству оказываемой услуги или
+                        сервису,
+                        пожалуйста, укажите телефон, оставленный вами при бронировании, чтобы мы могли уточнить
+                        обстоятельства и
+                        дать
+                        конкретный ответ по вашему вопросу. Мы гарантируем конфиденциальность ваших личных данных.
+                        Благодарим за
+                        понимание.
+                    </p>
+                    <p className={styles.info2}>
+                        Обработка отзывов осуществляется только в рабочие дни и может занять до 7 дней.
+                    </p>
+                </form>
+            )
             }
         </>
     );
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,34 +1,23 @@
 import React, {useEffect, useRef, useState} from 'react';
 import s from './Reviews.module.css';
-import useFetch from "../../hooks/useFetch";
-import useFetchAllData from "../../hooks/useFetchAllData";
-import {FaRegStar} from "react-icons/fa6";
-import ReviewForm from "./ReviewForm/ReviewForm";
+import useFetch from '../../hooks/useFetch';
+import useFetchAllData from '../../hooks/useFetchAllData';
+import {FaRegStar} from 'react-icons/fa6';
+import ReviewForm from './ReviewForm/ReviewForm';
+import {Preloader} from '../../common/Preloader/Preloader';
 
 const Reviews = () => {
-
     const [reviewCount, setReviewCount] = useState(5);
     const [loadingMore, setLoadingMore] = useState(false);
-    const [allLoaded, setAllLoaded] = useState(false); // Состояние для отслеживания загрузки всех отзывов
-    const reviewsContainerRef = useRef(null); // Реф для контейнера отзывов
+    const [allLoaded, setAllLoaded] = useState(false);
+    const reviewsContainerRef = useRef(null);
     const [isFormVisible, setIsFormVisible] = useState(false);
 
-    const {data: d, loading, error} = useFetch(`/reviews?populate=*`);
-
-    const {
-        data: c,
-        loading: l,
-        error: e
-    } = useFetchAllData(`/comments?fields[0]=name&fields[1]=comment&fields[2]=createdAt&fields[3]=grade&populate=*`);
-
-
-    console.log(c)
+    const {data: dataMedia, loading: loadingMedia, error: errorMedia} = useFetch(`/reviews?populate=*`);
+    const {data: comments, loading: commentsLoading, error: commentsError} = useFetchAllData(`/comments?populate=*`);
 
     const handleScroll = () => {
-        if (
-            reviewsContainerRef.current &&
-            reviewsContainerRef.current.getBoundingClientRect().bottom <= window.innerHeight
-        ) {
+        if (reviewsContainerRef.current && reviewsContainerRef.current.getBoundingClientRect().bottom <= window.innerHeight) {
             setLoadingMore(true);
             setReviewCount(prevCount => prevCount + 5);
         }
@@ -39,66 +28,91 @@ const Reviews = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-    // Обновляем состояние allLoaded, когда все отзывы загружены
     useEffect(() => {
-        if (c.length <= reviewCount) {
-            setAllLoaded(true);
-        } else {
-            setAllLoaded(false);
-        }
-    }, [c, reviewCount]);
+        setAllLoaded(comments.length <= reviewCount);
+    }, [comments, reviewCount]);
 
     return (
         <div>
-            <div className={s.reviewsWrapper}
-                 style={{
-                     backgroundImage: `url(${process.env.REACT_APP_UPLOAD_URL + d?.attributes?.imgBG?.data?.attributes?.url})`,
-                 }}
-            >
-                <h1>{d?.attributes?.title}</h1>
-                <p>{d?.attributes?.desc}</p>
-            </div>
+            {errorMedia ? (
+                <p>Что-то пошло не так</p>
+            ) : loadingMedia ? (
+                <Preloader/>
+            ) : (
+                <div
+                    className={s.reviewsWrapper}
+                    style={{
+                        backgroundImage: `url(${process.env.REACT_APP_UPLOAD_URL + dataMedia?.attributes?.imgBG?.data?.attributes?.url})`,
+                    }}
+                >
+                    <h1>{dataMedia?.attributes?.title}</h1>
+                    <p>{dataMedia?.attributes?.desc}</p>
+                </div>
+            )}
 
             <div ref={reviewsContainerRef} className={s.reviewsBlock}>
-                <h1 className={s.reviewTitle}>Отзывы - путь к лучшему опыту <span>{c?.length}</span></h1>
-                <button
-                    style={{display: isFormVisible ? 'none' : ''}}
-                    onClick={() => setIsFormVisible(true)}
-                    className={s.addReview}
-                >
-                    Добавить отзыв
-                </button>
-                {isFormVisible && < ReviewForm/>}
-                <div>
-                    {c.slice(0, reviewCount).map((com) => (
-                        <div key={com.id} className={s.reviewItem}>
-                            <div>
-                                {Array.from({length: 5}, (_, index) => (
-                                    <FaRegStar
-                                        className={s.grade}
-                                        key={index}
-                                        color={index < com?.attributes?.grade ? '#ffc107' : 'gray'}
-                                        size={20}
-                                    />
-                                ))}
+                <h1 className={s.reviewTitle}>
+                    Отзывы - путь к лучшему опыту <span>{comments?.length}</span>
+                </h1>
+                {!isFormVisible && (
+                    <button onClick={() => setIsFormVisible(true)} className={s.addReview}>
+                        Добавить отзыв
+                    </button>
+                )}
+                {isFormVisible && <ReviewForm/>}
+
+                {commentsError ? (
+                    <p>Что-то пошло не так</p>
+                ) : commentsLoading ? (
+                    <Preloader/>
+                ) : (
+                    comments
+                        .slice(-reviewCount)
+                        .reverse()
+                        .map(com => (
+                            <div key={com.id} className={s.reviewItem}>
+                                <div>
+                                    {Array.from({length: 5}, (_, index) => (
+                                        <FaRegStar
+                                            className={s.grade}
+                                            key={index}
+                                            color={index < com?.attributes?.grade ? '#ffc107' : 'gray'}
+                                            size={20}
+                                        />
+                                    ))}
+                                </div>
+                                <h3>{com?.attributes?.name}</h3>
+                                <span>
+                                    {com?.attributes?.createdAt &&
+                                        new Date(com.attributes.createdAt).toLocaleDateString('ru-RU', {
+                                            day: 'numeric',
+                                            month: 'long',
+                                            year: 'numeric',
+                                        })}
+                                </span>
+                                <p>{com?.attributes?.comment}</p>
                             </div>
-                            <h3>{com?.attributes?.name}</h3>
-                            <span>
-                                {com?.attributes?.createdAt
-                                    ? new Date(com.attributes.createdAt).toLocaleDateString('ru-RU', {
-                                        day: 'numeric',
-                                        month: 'long',
-                                        year: 'numeric',
-                                    })
-                                    : null}
-                            </span>
-                            <p>{com?.attributes?.comment}</p>
-                        </div>
-                    ))}
-                    {!allLoaded && loadingMore &&
-                        <p>Загрузка...</p>} {/* Показываем индикатор загрузки, если не все отзывы загружены */}
-                </div>
+                        ))
+                )}
+                {!allLoaded && loadingMore && <Preloader/>}
             </div>
+
+            {errorMedia ? (
+                <p>Что-то пошло не так</p>
+            ) : loadingMedia ? (
+                <Preloader/>
+            ) : (
+                <div className={s.reviewsRules}>
+                    <h2>{dataMedia?.attributes?.reviewRuleTitle}</h2>
+                    <ul>
+                        {dataMedia?.attributes?.reviewRuleDesc.map((item, index) => (
+                            item?.children.map((i, idx) => <li key={idx}>{i?.text}</li>)
+                        ))}
+                    </ul>
+                    <p>{dataMedia?.attributes?.reviewRuleConclusion_1}</p>
+                    <p>{dataMedia?.attributes?.reviewRuleConclusion_2}</p>
+                </div>
+            )}
         </div>
     );
 };
